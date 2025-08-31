bceb3c0e516cfc82a855bf945bc315037b4019b2
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-08-30 23:20:40 -0700

    Atomic operations and locking critical sections

---
+++ Java-Threads-Perf/src/PreventDeadLocks.java
@@ -0,0 +1,100 @@
+import java.util.Random;
+
+/*
+ * This program demonstrates how to prevent deadlocks in a multi-threaded environment.
+ * Two trains (TrainA and TrainB) are trying to pass through an intersection that has two roads (roadA and roadB).
+ * Each train needs to lock both roads to pass through the intersection.
+ * The program uses synchronized blocks to ensure that the locks are acquired in a consistent order,
+ * and lock acquisition order should be the same (This prevents deadlocks from occurring).
+ */
+
+public class PreventDeadLocks {
+
+    public static void main(String[] args) {
+        Intersection intersection = new Intersection();
+        Thread trainAThread = new Thread(new TrainA(intersection));
+        Thread trainBThread = new Thread(new TrainB(intersection));
+
+        trainAThread.start();
+        trainBThread.start();
+    }
+
+    public static class TrainB implements Runnable {
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainB(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepingTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTime);
+                } catch (InterruptedException e) {
+                }
+
+                intersection.takeRoadB();
+            }
+        }
+    }
+
+    public static class TrainA implements Runnable {
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainA(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepingTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTime);
+                } catch (InterruptedException e) {
+                }
+
+                intersection.takeRoadA();
+            }
+        }
+    }
+
+    public static class Intersection {
+        private Object roadA = new Object();
+        private Object roadB = new Object();
+
+        public void takeRoadA() {
+            synchronized (roadA) {
+                System.out.println("Road A is locked by thread " + Thread.currentThread().getName());
+
+                synchronized (roadB) {
+                    System.out.println("Train is passing through road A");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+
+        public void takeRoadB() {
+            synchronized (roadA) {
+                System.out.println("Road A is locked by thread " + Thread.currentThread().getName());
+
+                synchronized (roadB) {
+                    System.out.println("Train is passing through road B");
+
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+    }
+
+}
